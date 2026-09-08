f4ab4396e904d75716ed8d9489a0371d98282530
andistrehle <andi_strehle@hotmail.com>
2026-09-08 15:42:18 +0200

    MDC Kalender: Zusatztermin siebziger am 2026-09-17 Neuer Eintrag. Hinweis: Vertretung Fiakerstüberl

--- data/kalender.ts
+++ data/kalender.ts
@@ -82,6 +82,14 @@ export const KALENDER: Terminaenderung[] = [
     "art": "absage",
     "time": null,
     "note": "Wg Urlaub"
+  },
+  {
+    "id": "2026-09-17-siebziger-zusatz",
+    "date": "2026-09-17",
+    "venueId": "siebziger",
+    "art": "zusatz",
+    "time": "19:30",
+    "note": "Vertretung Fiakerstüberl"
   }
 ];
 
