d2b5719642399ae68f485c910bd93c0a47bac920
andistrehle <andi_strehle@hotmail.com>
2026-09-10 09:52:08 +0200

    MDC Kalender: Zusatztermin legendary am 2026-09-10 Neuer Eintrag.

--- data/kalender.ts
+++ data/kalender.ts
@@ -68,6 +68,14 @@ export const KALENDER: Terminaenderung[] = [
     "note": "Toni im Urlaub"
   },
   {
+    "id": "2026-09-10-legendary-zusatz",
+    "date": "2026-09-10",
+    "venueId": "legendary",
+    "art": "zusatz",
+    "time": "20:00",
+    "note": null
+  },
+  {
     "id": "2026-09-10-siebziger-zusatz",
     "date": "2026-09-10",
     "venueId": "siebziger",
